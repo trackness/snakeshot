4b2993da307e6eba00bb2f4d5505b3884ff7b023
trackness <40263851+trackness@users.noreply.github.com>
2021-07-02 13:58:11 +0100

    player redefined to be data source

---
+++ snakeshot/data/player.py
@@ -0,0 +1,48 @@
+class Player:
+
+    def __init__(
+            self,
+            _id: int = None,
+            gender: str = None,
+            first_name: str = None,
+            last_name: str = None,
+            full_name: str = None,
+            nationality: str = None
+    ):
+        self._id = _id
+        if gender in {"M", "H"}:
+            self._gender = "M"
+        if gender in {"W", "D"}:
+            self._gender = "W"
+        self._first_name: str = first_name
+        self._last_name: str = last_name
+        self._full_name: str = full_name
+        self._nationality: str = nationality
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def gender(self) -> str:
+        return self._gender
+
+    @property
+    def first_name(self) -> str:
+        return self._first_name
+
+    @property
+    def last_name(self) -> str:
+        return self._last_name
+
+    @property
+    def full_name(self) -> str:
+        return self._full_name
+
+    @property
+    def nationality(self) -> str:
+        return self._nationality
+
+    @classmethod
+    def qualifier(cls):
+        return Player(first_name="Q", last_name="Q", full_name="Q Q", nationality="-Q-")
--- snakeshot/tournament/player.py
+++ snakeshot/tournament/player.py
@@ -1,43 +1,22 @@
 from decimal import Decimal
 from statistics import mean
 
+from snakeshot.data import player
 
-class Player:
 
-    __slots__ = "_name_abbreviated", "_name_full", "_nationality", "_seed", "_entry_type", "_odds", "_rank"
-
+class Player(player.Player):
     def __init__(
             self,
-            name_abbreviated: str = None,
-            name_full: str = None,
-            nationality: str = None,
+            p: player.Player,
             seed: int = None,
             entry_type: str = None,
             rank: int = None,
     ):
-        self._name_abbreviated: str = name_abbreviated
-        self._name_full: str = name_full
-        self._nationality: str = nationality
+        super().__init__(**p.__dict__)
         self._seed: int = seed
         self._entry_type: str = entry_type
         self._odds: list[Decimal] = []
         self._rank: int = rank
-
-    @classmethod
-    def qualifier(cls):
-        return Player(name_abbreviated="Q", name_full="Q", nationality="-Q-")
-
-    @property
-    def name_abbreviated(self):
-        return self._name_abbreviated
-
-    @property
-    def name_full(self):
-        return self._name_full
-
-    @property
-    def nationality(self):
-        return self._nationality
 
     @property
     def seed(self):
@@ -62,3 +41,6 @@ class Player:
     @rank.setter
     def rank(self, value):
         self._rank = value
+
+    def source_rank(self):
+        self.rank = 0
