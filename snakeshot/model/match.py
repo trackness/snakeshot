0a51807a6289c8f713261030674d5ca07f11ce8a
trackness <40263851+trackness@users.noreply.github.com>
2021-07-04 00:24:23 +0100

    match actual winner removed for current scope

--- snakeshot/model/match.py
+++ snakeshot/model/match.py
@@ -3,10 +3,9 @@ from snakeshot.model.player import Player
 
 # TODO : Change to two players instead of list, just for init
 class Match:
-    def __init__(self, players: list[Player], winner_actual: Player = None):
+    def __init__(self, players: list[Player]):
         self._players: list[Player] = players
         self._winner_expected: int = self._set_winner_expected()
-        self._winner_actual: Player = winner_actual
 
     @property
     def winner_expected(self) -> Player:
