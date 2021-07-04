d3bcbbb05689cbc3bad457036b55cb83e10e4003
trackness <40263851+trackness@users.noreply.github.com>
2021-07-04 18:29:34 +0100

    atp started

--- snakeshot/model/slam.py
+++ snakeshot/model/slam.py
@@ -1,10 +1,68 @@
+import itertools
+from abc import ABC
+
+import cloudscraper as cloudscraper
+import requests as requests
+from bs4 import BeautifulSoup
 from snakeshot.model.player import Player
-from snakeshot.model.tournament import Tournament
 
 
-class Slam:
-    def __init__(self, players: dict[str, list[Player]]):
-        self._tournaments: dict[str, Tournament] = {
-            "Mens": Tournament(players.get("Mens")),
-            "Womens": Tournament(players.get("Womens"))
+class Slam(ABC):
+    def __init__(self):
+        self._player_pools: dict[str, list[Player]] = {
+            "M": Slam._mens_200(),
+            "W": Slam._womens_200(),
         }
+        Slam._set_odds(Slam._scrape_odds_list(), self._player_pools)
+
+    @classmethod
+    def _mens_200(cls) -> list[Player]:
+        query = "https://www.atptour.com/en/rankings/singles?&rankRange=1-200"
+        response = cloudscraper.create_scraper().get(query)
+        table = BeautifulSoup(response.text, "html.parser").find(
+            "table", {"class": "mega-table"}
+        )
+        return [Slam._atp_to_player(player) for player in table.findAll("tr")]
+
+    @classmethod
+    def _atp_to_player(cls, player) -> Player:
+        return Player(
+            _id=player.find,
+            gender="W",
+            first_name=player.get("player").get("firstName"),
+            last_name=player.get("player").get("lastName"),
+            full_name=player.get("player").get("fullName"),
+            nationality=player.get("player").get("countryCode"),
+            rank=player.get("ranking"),
+        )
+
+    @classmethod
+    def _womens_200(cls) -> list[Player]:
+        pages = [
+            f"https://api.wtatennis.com/tennis/players/ranked?page={i}&pageSize=100&type=rankSingles&metric=SINGLES"
+            for i in range(2)
+        ]
+        results = [requests.get(page).json() for page in pages]
+        return [
+            Slam._wta_to_player(player) for player in list(itertools.chain(*results))
+        ]
+
+    @classmethod
+    def _wta_to_player(cls, player: dict) -> Player:
+        return Player(
+            _id=player.get("player").get("id"),
+            gender="W",
+            first_name=player.get("player").get("firstName"),
+            last_name=player.get("player").get("lastName"),
+            full_name=player.get("player").get("fullName"),
+            nationality=player.get("player").get("countryCode"),
+            rank=player.get("ranking"),
+        )
+
+    @classmethod
+    def _scrape_odds_list(cls):
+        pass
+
+    @classmethod
+    def _set_odds(cls, odds, players):
+        pass
--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -1,21 +1,15 @@
+import pytest as pytest
+
 from snakeshot.model.match import Match
 from snakeshot.model.player import Player
 from snakeshot.model.round import Round
+from snakeshot.model.slam import Slam
 from snakeshot.model.tournament import Tournament
-
-standard_player: dict = {
-    "_id": 1,
-    "gender": "M",
-    "first_name": "A",
-    "last_name": "B",
-    "full_name": "A B",
-    "nationality": "ABC",
-}
 
 
 class TestPlayer:
     def test_odds(self):
-        player = Player(**standard_player, rank=1)
+        player = Player(1, "M", "A", "B", "A B", "ABC", 1)
         player.odds = 1.5
         player.odds = 3.0
         assert player.odds == 2.25
@@ -30,41 +24,53 @@ class TestPlayer:
 
 
 class TestMatch:
-    def test_winner_odds(self):
-        p1 = Player(**standard_player, rank=1)
-        p2 = Player(**standard_player, rank=2)
+    @pytest.fixture
+    def p1(self):
+        return Player(1, "M", "A", "B", "A B", "ABC", 1)
+
+    @pytest.fixture
+    def p2(self):
+        return Player(1, "M", "A", "B", "A B", "ABC", 2)
+
+    def test_winner_odds(self, p1, p2):
         p1.odds = 2
         p2.odds = 1
         assert Match([p1, p2]).winner_expected == p2
 
-    def test_winner_odds_equal(self):
-        p1 = Player(**standard_player, rank=1)
-        p2 = Player(**standard_player, rank=2)
+    def test_winner_odds_equal(self, p1, p2):
         p1.odds = 1
         p2.odds = 1
         assert Match([p1, p2]).winner_expected == p1
 
-    def test_winner_rank(self):
-        p1 = Player(**standard_player, rank=1)
-        p2 = Player(**standard_player, rank=2)
+    def test_winner_rank(self, p1, p2):
         assert Match([p1, p2]).winner_expected == p1
 
 
 class TestRound:
-    # TODO : parameterize this
-    def test_winners_1(self):
-        p1 = Player(**standard_player, rank=1)
-        p2 = Player(**standard_player, rank=2)
-        p3 = Player(**standard_player, rank=3)
-        p4 = Player(**standard_player, rank=4)
+    def test_winners(self):
+        p1 = Player(1, "M", "A", "B", "A B", "ABC", 1)
+        p2 = Player(2, "M", "A", "B", "A B", "ABC", 2)
+        p3 = Player(3, "M", "A", "B", "A B", "ABC", 3)
+        p4 = Player(4, "M", "A", "B", "A B", "ABC", 4)
         assert Round([Match([p1, p2]), Match([p3, p4])]).winners == [p1, p3]
 
 
 class TestTournament:
     def test_winner(self):
-        players = [Player(**standard_player, rank=i + 1) for i in range(8)]
+        players = [Player(i + 1, "M", "A", "B", "A B", "ABC", i + 1) for i in range(8)]
         assert Tournament(players).rounds[-1].winners == [players[0]]
 
     def test_number_of_rounds(self):
-        players = [Player(**standard_player, rank=i + 1) for i in range(16)]
+        players = [Player(i + 1, "M", "A", "B", "A B", "ABC", i + 1) for i in range(16)]
         assert Tournament(players)._n_rounds == 4
+
+
+class TestSlam:
+    def test_mens_200(self):
+        assert len(Slam._mens_200()) == 200
+
+    def test_womens_200(self):
+        assert len(Slam._womens_200()) == 200
+
+    def test_scrape_odds_list(self):
+        pass
