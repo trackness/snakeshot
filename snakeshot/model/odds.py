f0b0a0ef4b1729713e654b1dc27008d9fbcf9f1a
trackness <40263851+trackness@users.noreply.github.com>
2021-07-06 23:21:13 +0100

    odds finished

--- snakeshot/model/odds.py
+++ snakeshot/model/odds.py
@@ -6,20 +6,24 @@ import requests as requests
 
 
 class Odds:
+    _USER_AGENT = (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+    )
+
     @classmethod
     def scrape(cls, slam: str, tour: str) -> dict[str, Decimal]:
+        tour = tour.lower()
         url = f"https://www.oddschecker.com/tennis/{slam}/{tour}/{tour}-{slam}/winner"
-        response = requests.get(
-            url,
-            headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
-            },
-        ).text
+        response = requests.get(url, headers={"User-Agent": Odds._USER_AGENT}).text
         soup: BeautifulSoup = BeautifulSoup(response, "html.parser")
         rows = soup.find("tbody", {"id": "t1"}).findAll(
             "tr", {"class": "diff-row evTabRow bc"}
         )
-        return {row["data-bname"]: mean(Odds._compile(row)) for row in rows}
+        return {
+            row["data-bname"]: mean(Odds._compile(row))
+            for row in rows
+            if len(Odds._compile(row)) != 0
+        }
 
     @classmethod
     def _compile(cls, row: BeautifulSoup) -> list[Decimal]:
