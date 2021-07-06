dcf4bb7523387f2e3ae87aa20820a13ebdb8e3b2
trackness <40263851+trackness@users.noreply.github.com>
2021-07-06 23:27:43 +0100

    slam and wimbledon finished

--- snakeshot/model/slam.py
+++ snakeshot/model/slam.py
@@ -1,29 +1,73 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from decimal import Decimal
+from urllib.error import HTTPError
 
+import requests as requests
+from fuzzywuzzy import process
+
+from snakeshot.model.odds import Odds
 from snakeshot.model.player import Player
 from snakeshot.model.tour import Tour
+from snakeshot.model.tournament import Tournament
 
 
 class Slam(ABC):
-    def __init__(self, config: dict, pool_depth: int = 1000):
-        self._player_pools: dict[str, list[Player]] = {
-            "M": Tour("ATP", pool_depth).players,
-            "W": Tour("WTA", pool_depth).players,
+    def __init__(self, name: str, depth: int = 500):
+        self._name = name
+        self._depth = depth
+        self._tournaments: dict[str, Tournament] = {
+            tour: Tournament(self._players(tour)) for tour in ["Mens", "Womens"]
         }
-        # odds: Odds(config.get("slam"), config.get("tour"))
-        Slam._set_odds(Slam._scrape_odds_list(), self._player_pools)
+
+    def _players(self, tour: str) -> list[Player]:
+        competitors: list[Player] = self._load_draw(tour)
+        player_pool: list[Player] = Tour(tour, self._depth).players
+        [Slam._add_rank(c, player_pool) for c in competitors]
+        odds: dict[str, Decimal] = Odds.scrape(self._name.lower(), tour)
+        [Slam._add_odds(competitors, *o) for o in odds.items()]
+        [c.summary() for c in competitors]
+        return competitors
+
+    @abstractmethod
+    def _load_draw(self, tour: str) -> list:
+        raise NotImplementedError
 
     @classmethod
-    def _scrape_odds_list(
-        cls,
-    ) -> dict[str, list]:
-        pass
+    def _request_json(cls, url: str) -> dict:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"
+        }
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return dict(response.json())
+        except HTTPError as e:
+            print(f"HTTP error occurred: {e}")
+        except Exception as e:
+            print(f"Other error occurred: {e}")
 
     @classmethod
-    def _set_odds(cls, odds, players):
-        pass
+    def _add_rank(cls, competitor: Player, player_pool: list[Player]) -> None:
+        for p in player_pool:
+            if p.full_name == competitor.full_name:
+                competitor.rank = p.rank
+                return
+        match_name = process.extractOne(
+            competitor.full_name, [p.full_name for p in player_pool]
+        )
+        competitor.rank = next(
+            p for p in player_pool if p.full_name == match_name[0]
+        ).rank
 
-    wim_m = "https://www.oddschecker.com/tennis/wimbledon/mens/mens-wimbledon/winner"
-    wim_w = (
-        "https://www.oddschecker.com/tennis/wimbledon/womens/womens-wimbledon/winner"
-    )
+    @classmethod
+    def _add_odds(cls, competitors: list[Player], player: str, odds: Decimal) -> None:
+        for c in competitors:
+            if c.full_name == player:
+                c.odds = odds
+                return
+        match_name = process.extractOne(player, [c.full_name for c in competitors])
+        next(c for c in competitors if c.full_name == match_name[0]).odds = odds
+
+    @property
+    def results(self) -> str:
+        pass
--- snakeshot/model/slams/wimbledon.py
+++ snakeshot/model/slams/wimbledon.py
@@ -1,6 +1,50 @@
+from snakeshot.model.player import Player
 from snakeshot.model.slam import Slam
 
 
 class Wimbledon(Slam):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, year: int, depth: int = 1000):
+        self._base_url = f"https://www.wimbledon.com/en_GB/scores/feeds/{year}/draws"
+        super().__init__("Wimbledon", depth)
+
+    @classmethod
+    def _tour(cls, tour: str) -> str:
+        return {"Mens": "MS", "Womens": "LS"}.get(tour)
+
+    def _load_draw(self, tour: str) -> list[Player]:
+        matches: list[dict] = self._matches_json(tour)
+        players = []
+        for i, match in enumerate(matches):
+            [
+                players.insert(
+                    i * 2 + p, Wimbledon._team_to_player(match.get(f"team{p + 1}"))
+                )
+                for p in range(2)
+            ]
+        return players
+
+    def _matches_json(self, tour) -> list[dict]:
+        index: list = Slam._request_json(f"{self._base_url}/draws.json").get("draws")
+        draw_url: str = next(
+            t for t in index if t.get("id") == Wimbledon._tour(tour)
+        ).get("feed_url")
+        draw_json: dict = Slam._request_json(draw_url)
+        return draw_json.get("matches")[:64]
+
+    @classmethod
+    def _team_to_player(cls, team: dict) -> Player:
+        player = Player(
+            first_name=team.get("firstNameA"),
+            last_name=team.get("lastNameA"),
+            # display_name=team.get("displayNameA"),
+            nationality=team.get("nationA"),
+        )
+        if team.get("seed"):
+            player.seed = team.get("seed")
+        if team.get("entryStatus"):
+            player.entry_type = team.get("entryStatus")
+        return player
+
+
+if __name__ == "__main__":
+    w = Wimbledon(2021)
