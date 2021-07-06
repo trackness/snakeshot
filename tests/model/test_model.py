6d8af8575b54e8300e7ba395fa219d664040162a
trackness <40263851+trackness@users.noreply.github.com>
2021-07-06 23:29:40 +0100

    old test resources reduced

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -68,7 +68,7 @@ class TestTournament:
 
 class TestSlam:
     def test_player_pool(self):
-        slam: Slam = Slam(300)
+        slam: Slam = Slam({"slam": "wimbledon", "tour": "mens"}, 300)
         assert len(slam._player_pools.get("M")) == 300
         assert len(slam._player_pools.get("W")) == 300
 
