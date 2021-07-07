5d98adb0dd4f6902e9fd323e7fb25b2bfad430d9
trackness <40263851+trackness@users.noreply.github.com>
2021-07-07 15:52:29 +0100

    odds scraping fixed

--- snakeshot/sources/odds.py
+++ snakeshot/sources/odds.py
@@ -3,7 +3,7 @@ from statistics import mean
 from urllib.error import HTTPError
 
 from bs4 import BeautifulSoup
-from requests import Session
+from requests import Session, Response
 
 from snakeshot.utils.session import get_session
 
@@ -13,11 +13,18 @@ class Odds:
     def scrape(cls, slam: str, tour: str) -> dict[str, Decimal]:
         tour = tour.lower()
         url = f"https://www.oddschecker.com/tennis/{slam}/{tour}/{tour}-{slam}/winner"
-        source = Odds._get_source(url)
-        soup: BeautifulSoup = BeautifulSoup(source, "html.parser")
-        rows = soup.find("tbody", {"id": "t1"}).findAll(
-            "tr", {"class": "diff-row evTabRow bc"}
-        )
+        response = Odds._get_source(url)
+        soup: BeautifulSoup = BeautifulSoup(response.text, "html.parser")
+        try:
+            table = soup.find("tbody", {"id": "t1"})
+        except Exception as e:
+            print(f"No odds table found: {e}")
+            return {}
+        try:
+            rows = table.findAll("tr", {"class": "diff-row evTabRow bc"})
+        except Exception as e:
+            print(f"No odds rows found: {e}")
+            return {}
         return {
             row["data-bname"]: mean(Odds._compile(row))
             for row in rows
@@ -25,12 +32,12 @@ class Odds:
         }
 
     @classmethod
-    def _get_source(cls, url: str) -> str:
+    def _get_source(cls, url: str) -> Response:
         session: Session = get_session()
         try:
-            response = session.get(url)
+            response: Response = session.get(url)
             response.raise_for_status()
-            return response.text
+            return response
         except HTTPError as e:
             print(f"HTTP error occurred: {e}")
         except Exception as e:
@@ -42,3 +49,8 @@ class Odds:
             Decimal(col["data-odig"])
             for col in row.findAll("td", {"class": "bc bs oi"})
         ]
+
+
+if __name__ == "__main__":
+    o = Odds.scrape("wimbledon", "mens")
+    [print(f"{k} - {float(v)}") for k, v in o.items()]
