052656b5dfdff19c08fd10432d15afb13aa0f4e0
trackness <40263851+trackness@users.noreply.github.com>
2021-07-07 11:50:50 +0100

    round dict added

--- snakeshot/model/round.py
+++ snakeshot/model/round.py
@@ -8,6 +8,9 @@ class Round:
         self._matches: list[Match] = matches
         self._winners: list[Player] = [match.winner_expected for match in self._matches]
 
+    def __dict__(self):
+        return {self._idx: [match.__dict__ for match in self._matches]}
+
     @property
     def idx(self) -> int:
         return self._idx
