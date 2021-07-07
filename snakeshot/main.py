78d0bd3c739b035a4eeaf382836a6d94af163750
trackness <40263851+trackness@users.noreply.github.com>
2021-07-07 20:42:52 +0100

    logging implementation started

--- snakeshot/main.py
+++ snakeshot/main.py
@@ -3,25 +3,32 @@ import json
 from snakeshot.model.slams.wimbledon import Wimbledon
 from snakeshot.utils.printer import Printer
 
-print("Loading function")
+from loguru import logger
+
+logger.info("Loading function")
 
 
 def lambda_handler(event, context):
+    slam_name = str(event.get("slam", "wimbledon")).lower()
+    year = int(event.get("year", 2021))
+    logger.info(f"message received: Slam={slam_name}, Year={year}")
     slams = {
         # "australian_open": AustralianOpen,
         # "roland_garros", RolandGarros,
         "wimbledon": Wimbledon,
         # "us_open": USOpen
     }
-    slam = slams.get(str(event.get("slam")).lower(), Wimbledon)(2021)
+    slam = slams.get(slam_name)(year)
     try:
         response = {"statusCode": 200}
         if event.get("type") == "table":
+            logger.info(f"Generating table for {slam} {year}")
             response.update({"headers": {"Content-Type": "text/html"}})
             response.update({"body": Printer.table(slam.tournaments)})
         else:
+            logger.info(f"Generating json for {slam} {year}")
             response.update({"headers": {"Content-Type": "application/json"}})
             response.update({"body": json.dumps(slam.__dict__())})
         return response
     except Exception as e:
-        raise Exception(f"Something went wrong: {e}")
+        logger.error(f"Unable to process event: {e}")
