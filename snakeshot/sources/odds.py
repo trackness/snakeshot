3e4f198aed0c601d9d7a3a5d861ec4165a6776ee
trackness <40263851+trackness@users.noreply.github.com>
2021-07-07 14:37:03 +0100

    session re-added

--- snakeshot/sources/odds.py
+++ snakeshot/sources/odds.py
@@ -1,8 +1,11 @@
 from decimal import Decimal
 from statistics import mean
+from urllib.error import HTTPError
 
 from bs4 import BeautifulSoup
-import requests as requests
+from requests import Session
+
+from snakeshot.utils.session import get_session
 
 
 class Odds:
@@ -14,8 +17,8 @@ class Odds:
     def scrape(cls, slam: str, tour: str) -> dict[str, Decimal]:
         tour = tour.lower()
         url = f"https://www.oddschecker.com/tennis/{slam}/{tour}/{tour}-{slam}/winner"
-        response = requests.get(url, headers={"User-Agent": Odds._USER_AGENT}).text
-        soup: BeautifulSoup = BeautifulSoup(response, "html.parser")
+        source = Odds._get_source(url)
+        soup: BeautifulSoup = BeautifulSoup(source, "html.parser")
         rows = soup.find("tbody", {"id": "t1"}).findAll(
             "tr", {"class": "diff-row evTabRow bc"}
         )
@@ -24,6 +27,18 @@ class Odds:
             for row in rows
             if len(Odds._compile(row)) != 0
         }
+
+    @classmethod
+    def _get_source(cls, url: str) -> str:
+        session: Session = get_session(headers={"User-Agent": Odds._USER_AGENT})
+        try:
+            response = session.get(url)
+            response.raise_for_status()
+            return response.text
+        except HTTPError as e:
+            print(f"HTTP error occurred: {e}")
+        except Exception as e:
+            print(f"Other error occurred: {e}")
 
     @classmethod
     def _compile(cls, row: BeautifulSoup) -> list[Decimal]:
