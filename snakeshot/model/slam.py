cddd5a5f25e0ccc87b28f34a5a5f25a4da5b676e
trackness <40263851+trackness@users.noreply.github.com>
2021-07-11 13:54:18 +0100

    handler isolated

--- snakeshot/model/slam.py
+++ snakeshot/model/slam.py
@@ -1,10 +1,12 @@
 from snakeshot.model.tournament import Tournament
+from loguru import logger
 
 
 class Slam:
     _assoc = {"Mens": "ATP", "Womens": "WTA"}
 
     def __init__(self, name: str, year: int, depth: int = 500):
+        logger.info(f"Generating {name} {year} tournament")
         self._name = name
         self._year = year
         self._depth = depth
@@ -14,6 +16,7 @@ class Slam:
         }
 
     def as_dict(self) -> dict:
+        logger.info(f"Generating {self._name} {self._year} dict")
         return {tour: t.as_dict() for tour, t in self._tournaments.items()}
 
     @property
