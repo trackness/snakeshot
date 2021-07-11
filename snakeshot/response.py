67ad0959c606083b9d94b7dd09caa30c222c7808
trackness <40263851+trackness@users.noreply.github.com>
2021-07-11 16:30:43 +0100

    bootstrap table

--- snakeshot/response.py
+++ snakeshot/response.py
@@ -1,6 +1,5 @@
 from jinja2 import Template, Environment, FileSystemLoader
 
-from snakeshot.model.slam import Slam
 from loguru import logger
 
 
@@ -9,28 +8,15 @@ class Renderer:
     def write(cls, name: str, year: int, slam: dict) -> str:
         logger.info(f"Generating {name} {year} html tables")
 
-        logger.debug("Loading template")
+        logger.info("Loading template")
         template: Template = Environment(loader=FileSystemLoader("./")).get_template(
             "template.html"
         )
 
-        logger.debug("Making template substitutions")
+        logger.info("Making template substitutions")
         subs = template.render(title=f"{name} {year}", slam=slam)
 
-        with open("index.html", "w") as f:
-            f.write(subs)
+        # with open("index.html", "w") as f:
+        #     f.write(subs)
 
         return subs
-
-
-# if __name__ == "__main__":
-#     print(
-#         Renderer.write(
-#             "Wimbledon",
-#             2021,
-#             {
-#                 "Mens": [0, 1, 2],
-#                 "Womens": [3, 4, 5],
-#             }
-#         )
-#     )
