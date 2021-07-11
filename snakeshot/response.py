ed2c60435ebe6528d42caca3c935c4916e0b0c6b
trackness <40263851+trackness@users.noreply.github.com>
2021-07-11 17:09:16 +0100

    manual archive for debug

--- snakeshot/response.py
+++ snakeshot/response.py
@@ -1,3 +1,5 @@
+import os
+
 from jinja2 import Template, Environment, FileSystemLoader
 
 from loguru import logger
@@ -9,14 +11,13 @@ class Renderer:
         logger.info(f"Generating {name} {year} html tables")
 
         logger.info("Loading template")
-        template: Template = Environment(loader=FileSystemLoader("./")).get_template(
-            "template.html"
-        )
+        template: Template = Environment(
+            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "./"))
+        ).get_template("template.html")
 
         logger.info("Making template substitutions")
-        subs = template.render(title=f"{name} {year}", slam=slam)
+        return template.render(title=f"{name} {year}", slam=slam)
 
         # with open("index.html", "w") as f:
         #     f.write(subs)
-
-        return subs
+        # return subs
