c01fbb931eea4fa6e909854d525f3fb871d82f4a
trackness <40263851+trackness@users.noreply.github.com>
2021-07-12 13:17:22 +0100

    tf integration added

--- terraform/api.tf
+++ terraform/api.tf
@@ -94,5 +94,6 @@ resource "aws_apigatewayv2_integration" "slam" {
   api_id      = aws_apigatewayv2_api.api.id
   integration_type = "AWS_PROXY"
 
+  integration_uri = aws_lambda_function.lambda.invoke_arn
   payload_format_version = "2.0"
 }
