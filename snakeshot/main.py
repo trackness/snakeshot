6dfa73291af7c0ab1251fba92f6f3ddf69e24f11
trackness <40263851+trackness@users.noreply.github.com>
2021-07-12 14:33:26 +0100

    qs params

--- snakeshot/main.py
+++ snakeshot/main.py
@@ -13,28 +13,28 @@ def lambda_handler(event, context):
     logger.remove()
     logger.add(sys.stderr, level="INFO")
 
-    slam = slam_name(event)
-    if not slam_name:
+    slam = validate_slam(event)
+    if not slam:
         return Response.failure("slam is undefined")
 
-    content = Response(slam, year(event))
-    return content.as_json() if resp_json(event) else content.as_tables()
+    content = Response(slam, validate_year(event))
+    return content.as_json() if validate_json(event) else content.as_tables()
 
 
-def slam_name(event: dict) -> str:
+def validate_slam(event: dict) -> str:
     slam = event.get("queryStringParameters", {}).get(
-        "slam", event.get("http", {}).get("path", False)
+        "slam", event.get("requestContext", {}).get("http", {}).get("path", False)
     )
     return (
         slam if slam in ["aus_open", "roland_garros", "wimbledon", "us_open"] else False
     )
 
 
-def year(event: dict):
+def validate_year(event: dict):
     return int(event.get("queryStringParameters", {}).get("year", date.today().year))
 
 
-def resp_json(event: dict) -> bool:
+def validate_json(event: dict) -> bool:
     return bool(event.get("queryStringParameters", {}).get("json", False))
 
 
